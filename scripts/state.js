c4675707469a9659b6c7a5e585daa95b594aed9c
Ianropke <ianropke@gmail.com>
2025-01-03 16:03:51 +0100

    Update state.js

--- scripts/state.js
+++ scripts/state.js
@@ -1,14 +1,15 @@
 // scripts/state.js
 export const gameState = {
     currentZone: null,
-    tasks: [],
-    activeTask: null,
+    tasks: [], // Backlog of tasks
+    activeTask: null, // Currently selected task
     employees: [],
     resources: {
         servers: 0,
         softwareLicenses: 0,
         officeSpace: 0
     },
+    contracts: [], // Contracts array
     scores: {
         totalScore: 0,
         currentMoney: 50000,
