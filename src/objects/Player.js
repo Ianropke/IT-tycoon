e09f8dd1876df0c8aa12db5ee870f10699e94bdf
Ianropke <ianropke@gmail.com>
2025-01-05 17:28:36 +0100

    Create Player.js

---
+++ src/objects/Player.js
@@ -0,0 +1,36 @@
+// src/objects/Player.js
+export default class Player {
+  constructor(scene, x, y, texture) {
+    this.scene = scene;
+    this.sprite = scene.physics.add.sprite(x, y, texture);
+    this.sprite.setCollideWorldBounds(true);
+    this.speed = 300;
+
+    // Draw player as a circle
+    const graphics = scene.add.graphics();
+    graphics.fillStyle(0xffffff, 1);
+    graphics.fillCircle(0, 0, 20);
+    graphics.generateTexture('player', 40, 40);
+    graphics.destroy();
+
+    this.sprite.setTexture('player');
+  }
+
+  update(cursors) {
+    const body = this.sprite.body;
+    body.setVelocity(0);
+
+    if (cursors.left.isDown) {
+      body.setVelocityX(-this.speed);
+    }
+    if (cursors.right.isDown) {
+      body.setVelocityX(this.speed);
+    }
+    if (cursors.up.isDown) {
+      body.setVelocityY(-this.speed);
+    }
+    if (cursors.down.isDown) {
+      body.setVelocityY(this.speed);
+    }
+  }
+}
