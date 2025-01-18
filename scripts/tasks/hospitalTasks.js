b8a19261c80b186dd00e065d87b408fa0b71e5e5
Ianropke <ianropke@gmail.com>
2025-01-18 19:16:03 +0100

    Create hospitalTasks.js

---
+++ scripts/tasks/hospitalTasks.js
@@ -0,0 +1,14 @@
+// scripts/tasks/hospitalTasks.js
+
+window.hospitalTasks = [
+  {
+    category: "hospital",
+    title: "Biokemi Lab-automatisering",
+    shortDesc: "Automatisere prøvehåndtering.",
+    logicLong: "Først Hospital, så Infrastruktur, så Cybersikkerhed, slut Dok.",
+    steps:[
+      // 4 step...
+    ]
+  },
+  // ... 9 more ...
+];
