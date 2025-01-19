b9a48a668e7dd5b341da179e7cf00c884ceb18c8
Ianropke <ianropke@gmail.com>
2025-01-19 11:46:25 +0100

    Delete scripts/vendor.js

--- scripts/vendor.js
+++
@@ -1,8 +0,0 @@
-// vendor.js
-
-// If you have any vendor-specific logic (SLA checks, etc.)
-// you can define it here. For now, let's keep it minimal.
-
-function openVendorUI(scene) {
-  console.log('Vendor UI could open here if needed...');
-}
